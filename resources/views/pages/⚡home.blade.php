a9885387f33d159578a2797b69aed8ed1ae99563
felipecarvalho-back <felipedev.co@gmail.com>
2026-07-03 23:33:54 -0300

    build: instalando o livewire e testando o tailwindcss

---
+++ resources/views/pages/⚡home.blade.php
@@ -0,0 +1,16 @@
+<?php
+
+use Livewire\Component;
+use Livewire\Attributes\Title;
+
+new #[Title('Home')] class extends Component
+{
+
+};
+?>
+
+<div>
+    <h1 class="text-3xl font-bold underline">
+        Hello world!
+    </h1>
+</div>
